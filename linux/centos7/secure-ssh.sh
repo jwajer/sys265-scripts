7ad6b8ddb35676bf428ee7f789885abdc9a7ddd2
jwajer <jackson.wajer@mymail.champlain.edu>
2019-02-27 20:40:49 -0500

    updating things

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -4,4 +4,20 @@
 #Adds a public key to that users authorized keys file
 #Removes roots ability to ssh in
 
-echo "testing 123"
+echo -e "What user do you want to add?: \c "
+read user
+sudo useradd $user
+
+echo $user was created
+
+sudo mkdir /home/$user/.ssh
+
+sudo cp /home/jackson/sys265-scripts/linux/public-keys/SYS265Test.pub /home/$user/.ssh/authorized_keys
+
+sudo chmod 700 /home/$user/.ssh
+sudo chmod 600 /home/$user/.ssh/authorized_keys
+sudo chown -R $user:user /home/$user/.ssh
+
+sudo printf' \n%s\n' 'PermitRootLogin no' >> /etc/ssh/sshd_config
+sudo systemctl restart sshd
+echo permit root login disabled
