640b552e21b9dc7d2edaca46e2dfdb2076e4e145
jwajer <jackson.wajer@mymail.champlain.edu>
2019-02-27 20:52:34 -0500

    Edited secure-ssh.sh to add comments

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -1,23 +1,27 @@
 #SECURE-SSH.SH
 #AUTHOR JWAJER
-#Creates a new ssh user called $1
+#Creates a new ssh user called $user
 #Adds a public key to that users authorized keys file
 #Removes roots ability to ssh in
 
+#Creating a user of a given username
 echo -e "What user do you want to add?: \c "
 read user
 sudo useradd $user
-
-echo $user was created
-
+echo user  $user was created
 sudo mkdir /home/$user/.ssh
 
-sudo cp /home/jackson/sys265-scripts/linux/public-keys/SYS265Test.pub /home/$user/.ssh/authorized_keys
+#Copies the public key from my directory to directory of the new user
+sudo cp /home/jackson/sys265-scripts/linux/public-keys/sys265.pub /home/$user/.ssh/authorized_keys
 
+#Modifies directory permissions appropriately
 sudo chmod 700 /home/$user/.ssh
 sudo chmod 600 /home/$user/.ssh/authorized_keys
 sudo chown -R $user:user /home/$user/.ssh
 
+#Disables root login
 sudo printf' \n%s\n' 'PermitRootLogin no' >> /etc/ssh/sshd_config
 sudo systemctl restart sshd
 echo permit root login disabled
+
+echo test123
